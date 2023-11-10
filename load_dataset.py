4860dcfd016b3f4ceb7fe684324b6d25f4e785e3
CJ4n <yancichomski@gmail.com>
2023-11-10 19:08:17 +0100

    save

--- load_dataset.py
+++
@@ -1,8 +0,0 @@
-import openml
-from pandas import DataFrame
-
-
-def load_dataset_from_id(id: int) -> DataFrame:
-    return openml.datasets.get_dataset(dataset_id=id).get_data(
-        dataset_format="dataframe"
-    )[0]
